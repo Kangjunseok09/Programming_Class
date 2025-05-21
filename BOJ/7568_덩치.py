e4c5cc33960a9fe193977265fe331e6c6a6c4227
강준석 <kangjunsuk0503@gmail.com>
2025-05-21 23:32:41 +0900

    0521 백준문제

--- BOJ/7568_덩치.py
+++ BOJ/7568_덩치.py
@@ -1,4 +1,5 @@
 import sys
+input = sys.stdin.readline
 n = int(input())
 input = sys.stdin.readline
 w = []
@@ -8,3 +9,12 @@ for i in range(n):
   a, b = map(int, input().split())
   w.append(a)
   h.append(b)
+for i in range(n):
+  rank = 1
+  for j in range(n):
+    if i != j:
+      if w[i] < w[j] and h[i] < h[j]:
+        rank += 1
+  grade.append(rank)
+for i in grade:
+  print(i, end=" ")
