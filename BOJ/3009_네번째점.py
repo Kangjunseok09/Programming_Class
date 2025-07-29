bc1cd7016ddbd42d02fa330bcf970caf43c1a808
강준석 <kangjunsuk0503@gmail.com>
2025-07-30 03:12:05 +0900

    백준 수정

--- BOJ/3009_네번째점.py
+++ BOJ/3009_네번째점.py
@@ -1,1 +1,8 @@
-coordinates = [map()]
+coordinates = [tuple(map(int, input().split())) for i in range(3)]
+x, y = zip(*coordinates)
+for i in x:
+  if x.count(i) == 1:
+    print(i, end=" ")
+for i in y:
+  if y.count(i) == 1:
+    print(i)
